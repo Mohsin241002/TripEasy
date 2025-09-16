3ea3175d903fa6932659a508741ac71f3f83655b
Mohsin241002 <chunawala246@gmail.com>
2025-09-16 23:23:59 +0530

    final key replacement

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB3OibZMyDuTbNZitNW-y1NH9uB1RfA_mE",
-  authDomain: "aitravelplanner-1607a.firebaseapp.com",
-  projectId: "aitravelplanner-1607a",
-  storageBucket: "aitravelplanner-1607a.firebasestorage.app",
-  messagingSenderId: "1075829852055",
-  appId: "1:1075829852055:web:0e7ad0f9f78f46b26be6d2",
-  measurementId: "G-NYCYTTT7K1"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
